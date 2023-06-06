6bf359eda0106bbd11260c3794608bad24178114
Tomáš Duchoslav <duchoslav.tomas@gmail.com>
2023-06-06 13:29:42 +0200

    Initial Commit

---
+++ FinalRohlik/FinalRohlikApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalRohlikApp.swift
+//  FinalRohlik
+//
+//  Created by Tomáš Duchoslav on 06.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FinalRohlikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
